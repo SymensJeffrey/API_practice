d1a499fa0f15844da4bf64346a03abe9d741e6da
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-10 19:43:27 -0500

    create and start weather app

---
+++ API-practice.rb
@@ -0,0 +1,39 @@
+p "Welcome to the weather app. Please enter the city:"
+city_answer = gets.chomp
+p "What units would you like"
+units_answer = gets.chomp
+
+
+
+require 'http'
+
+response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city_answer.downcase}&units=imperial&appid=#{ENV['OPEN_WEATHER_API_KEY']}")
+
+weather_data = response.parse(:json)
+temperature = weather_data['main']['temp']
+
+pp weather_data
+
+
+# Some features to add to your weather app:
+# Ask the user for the name of a city to forecast
+# Ask the user for preferred temperature units (Celcius or Farenheit)
+# Write the app in a loop (once you show the forecast, the user can keep entering different cities until they choose to quit the program)
+
+#----------------------------------
+
+# It is currently 75 Degrees and sunny in Buffalo
+
+# require 'http'
+
+# response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=buffalo&appid=#{ENV['OPEN_WEATHER_API_KEY']}&units=imperial")
+
+# weather_data = response.parse(:json)
+# temperature = weather_data['main']['temp']
+# description = weather_data['weather'][0]['description']
+# city_name = weather_data['name']
+
+
+# system "clear"
+# p "welcome to the weather app!"
+# p "It is #{temperature} degrees and #{description} in #{city_name}" 
