cca14ab773d1c540aed5f356027e9f145a64ebe6
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-10 20:16:36 -0500

    make infinite loop

--- API-practice.rb
+++ API-practice.rb
@@ -1,29 +1,37 @@
-system "clear"
-p "Welcome to the weather app. Please enter the city:"
-city_answer = gets.chomp
-p "What units would you like"
-units_answer = gets.chomp
+while true
+  system "clear"
+  p "Welcome to the weather app. Please enter the city: (enter 'q' to quit)"
+ 
+  city_answer = gets.chomp
 
-if units_answer.downcase == "farenheit"
-  unit = "imperial"
-elsif units_answer.downcase == "celcius"
-  unit = "metric"
-elsif units_answer.downcase == "kelvin"
-  unit = "standard"
+  if city_answer == 'q'
+    break
+  end
+  
+  p "What units would you like"
+  units_answer = gets.chomp
+
+  if units_answer.downcase == "farenheit"
+    unit = "imperial"
+  elsif units_answer.downcase == "celcius"
+    unit = "metric"
+  elsif units_answer.downcase == "kelvin"
+    unit = "standard"
+  end
+
+
+  require 'http'
+
+  response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city_answer.downcase}&units=#{unit}&appid=#{ENV['OPEN_WEATHER_API_KEY']}")
+
+  weather_data = response.parse(:json)
+  temperature = weather_data['main']['temp']
+  description = weather_data['weather'][0]['description']
+  city_name = weather_data['name']
+
+  p "It is currently #{temperature} degrees #{units_answer} and #{description} in #{city_name}."
+
 end
-
-
-require 'http'
-
-response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city_answer.downcase}&units=#{unit}&appid=#{ENV['OPEN_WEATHER_API_KEY']}")
-
-weather_data = response.parse(:json)
-temperature = weather_data['main']['temp']
-description = weather_data['weather'][0]['description']
-city_name = weather_data['name']
-
-p "It is currently #{temperature} degrees #{units_answer} and #{description} in #{city_name}."
-
 
 # Some features to add to your weather app:
 # Ask the user for the name of a city to forecast
